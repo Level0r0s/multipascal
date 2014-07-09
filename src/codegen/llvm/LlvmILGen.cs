c1bad5f8ce963e17d5a74fc367f2309a53d1ff0f
Miguel Ferreira <eligmu@gmail.com>
2014-07-09 15:32:49 +0100

    Fixed spelling error of ArithmeticBinaryExpression

--- src/codegen/llvm/LlvmILGen.cs
+++ src/codegen/llvm/LlvmILGen.cs
@@ -534,7 +534,7 @@ namespace MultiPascal.Codegen.LlvmIL
 		}
 		
 		// Currently only working with ints
-		public override Value Visit(ArithmethicBinaryExpression node)
+		public override Value Visit(ArithmeticBinaryExpression node)
 		{
 			Visit((BinaryExpression)node);
 			Value l = traverse(node.left);
